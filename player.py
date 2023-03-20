e559c35f39fc34c1575e07d6df17298bf3e93b75
Muhammad Ashhad Safwan <m_ashhadhlr@yahoo.com>
2023-03-21 00:26:17 +0500

    Necessary indentation

--- player.py
+++ player.py
@@ -42,7 +42,7 @@ class GeniusComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        if len(game.available_moves()) ==9:
+        if len(game.available_moves()) == 9:
             square = random.choice(game.available_moves())
         else:
             square = self.minimax(game, self.letter)['position']
@@ -50,13 +50,13 @@ class GeniusComputerPlayer(Player):
     
     def minimax(self, state, player):
         max_player = self.letter 
-        other_player = 'O'if player== 'X' else 'X'
+        other_player = 'O' if player == 'X' else 'X'
 
         if state.current_winner == other_player:
-            return {'position' : None,'score' : 1*(state.num_empty_squares()+1) if other_player == max_player else -1 *(state.num_empty_squares()+1)}
+            return {'position' : None, 'score' : 1*(state.num_empty_squares()+1) if other_player == max_player else -1 *(state.num_empty_squares()+1)}
             
         elif not state.empty_squares():
-            return {'position':None, 'score': 0}
+            return {'position': None, 'score': 0}
         
         if player == max_player:
             best = {'position': None, 'score': -math.inf} #each score to be maximised
@@ -64,15 +64,20 @@ class GeniusComputerPlayer(Player):
             best = {'position': None, 'score': math.inf} #each score to be minimised
     
         for possible_move in state.available_moves():
-            sim_score = self.minimax(state, other_player)
-            state.board[possible_move]= ' '
-            state.current_winner = None
-            sim_score['position'] = possible_move
+            state.make_move(possible_move, player)
+            sim_score = self.minimax(state, other_player)  # simulate a game after making that move
 
-        if player == max_player:
-            if sim_score['score'] > best ['score']:
-                best = sim_score
-        else: 
-            if sim_score['score'] < best ['score']:
-                best = sim_score
+            # undo move
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            sim_score['position'] = possible_move  # this represents the move optimal next move
+
+            if player == max_player:
+                if sim_score['score'] > best ['score']:
+                    best = sim_score
+            else: 
+                if sim_score['score'] < best ['score']:
+                    best = sim_score
+        
+        return best
         
