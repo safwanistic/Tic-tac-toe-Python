e13fe08c70b5fd79a9757115d11fcea838b09f9d
Muhammad Ashhad Safwan <m_ashhadhlr@yahoo.com>
2023-03-20 18:12:53 +0500

    Update game.py

--- game.py
+++ game.py
@@ -77,7 +77,7 @@ def play(game, x_player, o_player, print_game = True):
             square = x_player.get_move(game)
         if game.make_move(square, letter):
             if print_game:
-                print(letter + f' makes a move to square {square}'.format(square))
+                print(letter + f' makes a move to square {square}')
                 game.print_board()
                 print('')
 
